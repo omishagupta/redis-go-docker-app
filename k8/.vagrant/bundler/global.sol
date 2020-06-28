65089220cb320d9391c43b481872b386cc9e71a8
omisha.gupta <omisha.gupta@atherenergy.com>
2020-06-29 02:59:50 +0530

    WIP: version 1 of the ops assignment

---
+++ k8/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["ffi",[">= 1.0.0"]],["win32-process",[">= 0"]],["vagrant-sshfs",["= 1.3.4"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.24.0"]]],"checksum":"c6a466f6ff7004fbc2d5797862945e2efeda41e02e2906a6dab31304cc159bd2","vagrant_version":"2.2.9"}
---
+++ vagrant-bootup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+echo "---------------------------- Checking dependencies -----------------------------"
+if [ -x "$(command -v vagrant)" ]; then
+    echo "Vagrant version: " `vagrant --version`
+else
+    apt install virtualbox
+    apt update
+    curl -O https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.deb
+    apt install ./vagrant_2.2.6_x86_64.deb
+fi
+
+if [ -x "$(command -v ansible)" ]; then
+    echo "Ansible version: " `ansible --version -`
+else
+    apt update
+    apt install software-properties-common
+    apt-add-repository --yes --update ppa:ansible/ansible
+    apt install ansible
+fi
+
+echo "-------------------------- Dependencies Resolved ----------------------------"
+
+echo "Which underneath service would you want to run the app on (choose the corresponding number): \n [1] Docker \n [2] Kubernetes "
+read choice
+if [[ $choice == 1 ]]; then
+    service=Docker
+else
+    service=Kubernetes
+fi
+
+export -n service
+echo "Bringing up vagrant cluster with $service ( This will take around 10 mins) "
+cd vagrant-ansible
+
+if [[ $service == "Docker" ]]; then
+    vagrant up docker
+else 
+    vagrant up kubernetes
+fi
